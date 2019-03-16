c7bfb519a5ed10774d50bf487b63aa205d0e9997
AliaksandrChachura <skif198560@gmail.com>
2019-03-16 09:31:22 +0300

    Correct decision

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-@param preferences - an array of integers. Indices of people, whom they love
-@returns number of love triangles
+/**
+ * @param preferences - an array of integers. Indices of people, whom they love
+ * @returns number of love triangles
+ */
 
 module.exports = function getLoveTrianglesCount(preferences = []) {
     let count = 0;
     for (let i=0;i < preferences.length; i++){
         let value = preferences[i];
-        //alert ("value = " + (i+1) + "  =  " + preferences[i]);
-        //alert ("preferences[value] = " + value + "    " + preferences[value - 1]);
         let value1 = preferences[value-1];
-        // alert ("value1 = " + value1 + "  = " + preferences[value1-1]);
 
 
 
-        if (i+1 === preferences[value1-1] ){
-            //alert((i+1) + " =  " + preferences[value1-1]);
-            count += 1;
-            alert ("count = " + count);
+        if (i+1 === preferences[value1-1]  && i+1 !== preferences[i] ){
+            count += 1
             preferences[i] = NaN;
             preferences[value-1] = NaN;
             preferences[value1-1] = NaN;
-            // alert (value1-1 + "  " + preferences[preferences[value1-1]]);
-
         }
 
     }
